5316417f71b995d5cdad51b28276af0d7efe5a95
Nicholas Tang <ntang11@ucsc.edu>
2025-10-05 20:30:29 -0700

    add math helper

---
+++ src/math_helper.h
@@ -0,0 +1,8 @@
+#ifndef MATH_HELPER_H
+#define MATH_HELPER_H
+
+void gaussian_dist_1d(float *weights, int radius, float sigma);
+
+void gaussian_dist_2d(float *weights, int radius, float sigma);
+
+#endif
